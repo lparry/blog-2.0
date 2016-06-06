13ed0946c6d541052f70782a584d53aa3a59d657
Lucas Parry <lparry@gmail.com>
2016-06-06 22:16:54 +1000

    Simpler nav

--- components/Navigation/index.jsx
+++ components/Navigation/index.jsx
@@ -9,9 +9,7 @@ import "./styles.scss"
 import Link from "../Link"
 
 const NavLink = ({ href, children }) => (
-  <span className="navigation navigationHeader">
-    <Link className="navigationLink" to={href} >{children}</Link>
-  </span>
+  <Link className="navigationLink" to={href} >{children}</Link>
 )
 NavLink.propTypes = {
   href: PropTypes.string.isRequired,
@@ -22,15 +20,14 @@ function Navigation() {
   return (
     <nav className="navbar" role="navigation">
       <div className="navContainer">
-        <span className="navigation">
-          <Link className="navigationLink" to="/"><i className="fa fa-globe fa-lg fa-fw" />Lucas The Nomad</Link>
-        </span>
+        <Link className="navigationLink" to="/"><i className="fa fa-globe fa-lg fa-fw" />Lucas The Nomad</Link>
         <NavLink href="https://www.facebook.com/lucasparry"><i className="fa fa-facebook fa-lg fa-fw" />Facebook</NavLink>
         <NavLink href="https://www.twitter.com/lucas_parry"><i className="fa fa-twitter fa-lg fa-fw" />Twitter</NavLink>
         <NavLink href="https://www.flickr.com/photos/lucasthenomad/"><i className="fa fa-flickr fa-lg fa-fw" />Flickr</NavLink>
         <NavLink href="https://instagram.com/lparry"><i className="fa fa-instagram fa-lg fa-fw" />Instagram</NavLink>
         <NavLink href="https://www.couchsurfing.org/people/lucasthenomad/"><i className="fa fa-suitcase fa-lg fa-fw" />Couchsurfing</NavLink>
         <NavLink href="/map"><i className="fa fa-map-marker fa-lg fa-fw" />Travel Map</NavLink>
+        <NavLink href="/tags">Tags</NavLink>
       </div>
     </nav>
   )
