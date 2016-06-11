3f4fda7ca639f554dd0e3cbbb110cb37e0907175
Lucas Parry <lparry@gmail.com>
2016-06-11 15:48:16 +1000

    Clean tags and pagination pages

--- tools/clean.js
+++ tools/clean.js
@@ -4,5 +4,7 @@ import fs from "./lib/fs"
 
 export default task(async function clean() {
   await del(["build/*", "!build/.git"], { dot: true })
+  await del(["pages/tags/*", "!pages/tags/.keep"], { dot: true })
+  await del(["pages/page/*", "!pages/page/.keep"], { dot: true })
   await fs.mkdir("build")
 })
