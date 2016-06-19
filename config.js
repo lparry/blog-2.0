5e778b0c3063784dd66b730b4dff442c46c25e8b
Lucas Parry <lparry@gmail.com>
2016-06-19 16:45:39 +1000

    Correct analytics details

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export default {
   title: "Lucas The Nomad",
-  description: "oh look, a description",
-  googleAnalyticsId: "UA-XXXXX-X",
+  description: "The story of an irresponsible thirty year old man-child who decided he didn't want to wait until he was old to have a turn at enjoying retirement. Instead of following the common life path of getting married, buying a house, and having two kids, he's chosen to seek out epic adventures, see the …",
+  googleAnalyticsId: "UA-8216018-4",
   disqusShortName: "lucasthenomad",
   perPage: 5,
 }
